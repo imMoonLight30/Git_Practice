6ebdf1c42dbf5c0a57790aabf53339f321b98606
imMoonLight30 <30cpss@gmail.com>
2023-08-26 07:01:06 +0530

    add time

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,11 @@
 //write program
 class helloWorld{
-
+    int time;
     public void show(){
         System.out.println("hello world");
+    }
+    public void addNew(){
+        time=1;
     }
     public static void main(String[] args){
         helloWorld obj=new helloWorld();
