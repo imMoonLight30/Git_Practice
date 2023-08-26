b2c07352999c11b9744814b5cc1a1eb82664fc87
imMoonLight30 <30cpss@gmail.com>
2023-08-26 07:05:27 +0530

    use add varialbe

--- helloWorld.java
+++ helloWorld.java
@@ -2,13 +2,14 @@
 class helloWorld{
     int time;
     public void show(){
-        System.out.println("hello world");
+        System.out.println("hello world - "+time);
     }
     public void addNew(){
         time=1;
     }
     public static void main(String[] args){
         helloWorld obj=new helloWorld();
+        obj.addNew();
         obj.show();
     }
     
